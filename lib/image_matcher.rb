ef910273e47e51e37f037e5602404ce7d0339090
indigolain <indigo.lain@gmail.com>
2019-08-03 22:14:42 +0900

    Initial commit

---
+++ lib/image_matcher.rb
@@ -0,0 +1,6 @@
+require "image_matcher/version"
+
+module ImageMatcher
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/image_matcher/version.rb
@@ -0,0 +1,3 @@
+module ImageMatcher
+  VERSION = "0.1.0"
+end
